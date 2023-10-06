b23a55a429c8a3fcedbfebd08f47d547ac85122a
Guillermo Díaz <guillermo@otherplane.com>
2023-10-06 13:38:20 +0200

    refactor: cross chain ots retrievals

--- migrations/witnet/retrievals/index.js
+++ migrations/witnet/retrievals/index.js
@@ -1,11 +1,12 @@
 const Witnet = require("witnet-utils")
 module.exports = {
     "Off-the-shelf": {
+        "Cross-chain": require('./cross-chain'),
         "De-Fi": {
             "Exchanges": require('./exchanges'),
             "DEXes": require('./dexes')
         },
         "Generics": require('./generics'),
-        "Web3": require('./web3')
+    },
     },
 }
