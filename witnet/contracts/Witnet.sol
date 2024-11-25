4f52b83e21086407f3f523248bb592c294091c03
Guillermo Díaz <guillermo@otherplane.com>
2024-11-25 21:02:23 +0100

    fix: import of WitMocked* contracts for tests

--- witnet/contracts/Witnet.sol
+++ witnet/contracts/Witnet.sol
@@ -6,5 +6,5 @@ import "witnet-solidity-bridge/contracts/mockups/UsingWitOracle.sol";
 import "witnet-solidity-bridge/contracts/WitPriceFeeds.sol";
 
 // Witnet mocking contracts (only for local/unitary testing):
-import "witnet-solidity-bridge/test/mocks/WitMockedOracle.sol";
-import "witnet-solidity-bridge/test/mocks/WitMockedPriceFeeds.sol";
+import "witnet-solidity-bridge/mockups/tests/WitMockedOracle.sol";
+import "witnet-solidity-bridge/mockups/tests/WitMockedPriceFeeds.sol";
