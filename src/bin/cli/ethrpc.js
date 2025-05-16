be6c4955d9069879b50e483685ed4bca1840fc9d
Guillermo Díaz <guillermo@witnet.io>
2025-05-16 13:55:12 +0200

    feat(cli): ethrpc: run on port 8545 if no otherwise specified

--- src/bin/cli/ethrpc.js
+++ src/bin/cli/ethrpc.js
@@ -8,23 +8,28 @@ const networks = require("./networks")
 
 module.exports = async function (flags = {}, args = []) {
     [args] = helpers.deleteExtraFlags(args)
-    const network = flags?.network || process.env.WITNET_SDK_SOLIDITY_NETWORK
-    if (!network || !supportsNetwork(network)) {
-        if (network) throw `Unsupported network "${network}"`
-        else throw "No EVM network was specified!";
+    const network = args[0]
+    if (!network) {
+        throw new Error("No EVM network was specified.")
+
+    } else if (network && !supportsNetwork(network)) {
+        throw new Error(`Unsupported network "${network}"`)
+    
+    } else {
+        const shell = spawn(
+            os.type() === "Windows_NT" ? "npx.cmd" : "npx", [
+            "ethrpc",
+            network,
+            flags?.port || 8545,
+            flags?.provider
+        ],
+            { shell: true, }
+        )
+        shell.stdout.on("data", (x) => {
+            process.stdout.write(x.toString())
+        })
+        shell.stderr.on("data", (x) => {
+            process.stderr.write(x.toString())
+        })
     }
-    const shell = spawn(
-        os.type() === "Windows_NT" ? "npx.cmd" : "npx", [
-        "ethrpc",
-        network,
-        flags?.provider
-    ],
-        { shell: true, }
-    )
-    shell.stdout.on("data", (x) => {
-        process.stdout.write(x.toString())
-    })
-    shell.stderr.on("data", (x) => {
-        process.stderr.write(x.toString())
-    })
 }
