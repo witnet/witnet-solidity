ad993584ae7be5c87c46f7a81950fec4806d51c9
Guillermo Díaz <guillermo@witnet.io>
2025-06-03 10:44:47 +0200

    fix: revisit package postinstall

--- src/bin/postinstall.js
+++ src/bin/postinstall.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const exec = require("child_process").exec
+const exec = require("child_process").execSync
 const fs = require("fs")
 
 if (!fs.existsSync(".no-postinstall")) {
-    // initialize Witnet Radon workspace, relying on @witnet/sdk-solidity instead of @witnet/sdk
-    exec(`npx witnet-solidity install --version`)
+    // initialize Witnet Radon workspace relying on @witnet/solidity instead of @witnet/sdk
+    exec(`node ./src/bin/solidity.js install --version`)
 }
